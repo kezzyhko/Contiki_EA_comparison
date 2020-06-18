1c207c0e1060b0010dff33bee4616a7df8d279fa
bogotolec <i.makarenko@innopolis.university>
2020-06-18 13:37:14 +0300

    fixed bugs

--- cryptolibs/aes.c
+++ cryptolibs/aes.c
@@ -9,13 +9,12 @@ unsigned long _rke[RKLENGTH(MAX_KEYBITS)];
 unsigned long _rkd[RKLENGTH(MAX_KEYBITS)];
 unsigned char _key[KEYLENGTH(MAX_KEYBITS)];
 int _keybits;
-int _nerounds;
-int _ndrounds;
+int _nrounds;
 
-int setKey(char* key, int keybits)
+void setKey(unsigned char* key, int keybits)
 {
-	_nerounds = rijndaelSetupEncrypt(_rke, key, keybits);
-	_ndrounds = rijndaelSetupDecrypt(_rkd, key, keybits);
+	_nrounds = rijndaelSetupEncrypt(_rke, key, keybits);
+	_nrounds = rijndaelSetupDecrypt(_rkd, key, keybits);
 
 	_keybits = keybits;
 	memcpy(_key, key, KEYLENGTH(keybits));
@@ -23,10 +22,10 @@ int setKey(char* key, int keybits)
 
 void encrypt(const unsigned char plaintext[16], unsigned char ciphertext[16])
 {
-	rijndaelEncrypt(_rke, _nerounds, plaintext, ciphertext);
+	rijndaelEncrypt(_rke, _nrounds, plaintext, ciphertext);
 }
 
 void decrypt(const unsigned char ciphertext[16], unsigned char plaintext[16])
 {
-	rijndaelDecrypt(_rkd, _ndrounds, ciphertext, plaintext);
+	rijndaelDecrypt(_rkd, _nrounds, ciphertext, plaintext);
 }
--- cryptolibs/aes.h
+++ cryptolibs/aes.h
@@ -1,7 +1,7 @@
-#ifndef CRYPTO_H
-#define CRYPTO_H
+#ifndef AES_H
+#define AES_H
 
-int setKey(char* key, int keybits);
+void setKey(char* key, int keybits);
 void encrypt(const unsigned char plaintext[16], unsigned char ciphertext[16]);
 void decrypt(const unsigned char ciphertext[16], unsigned char plaintext[16]);
 
