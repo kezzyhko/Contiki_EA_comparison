bd20d6b0f7c34f4e5b69364c3f42cba2278c27bb
bogotolec <i.makarenko@innopolis.university>
2020-06-23 19:43:55 +0300

    Small optimizations

--- cryptolibs/aes.c
+++ cryptolibs/aes.c
@@ -9,8 +9,8 @@ unsigned long _rke[RKLENGTH(MAX_KEYBITS)];
 unsigned long _rkd[RKLENGTH(MAX_KEYBITS)];
 int _nrounds;
 
-unsigned char _ive[32] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10 };
-unsigned char _ivd[32] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10 };
+unsigned char _ive[16] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10 };
+unsigned char _ivd[16] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10 };
 
 void setKey(unsigned char* key, int keybits)
 {
@@ -18,10 +18,10 @@ void setKey(unsigned char* key, int keybits)
 	_nrounds = rijndaelSetupDecrypt(_rkd, key, keybits);
 }
 
-void xor(unsigned char a[16], const unsigned char b[16]) {
-	int i = 17;
+void xor(unsigned int* a, const unsigned int* b) {
+	int i = 5;
 	while (--i > 0) {
-		*a^=*b;
+		*a ^= *b;
 		++a;
 		++b;
 	}
@@ -29,7 +29,7 @@ void xor(unsigned char a[16], const unsigned char b[16]) {
 
 void encrypt(const unsigned char plaintext[16], unsigned char ciphertext[16])
 {
-	xor(_ive, plaintext);
+	xor( (unsigned int*) _ive, (unsigned int*) plaintext);
 	rijndaelEncrypt(_rke, _nrounds, _ive, ciphertext);
 	memcpy(_ive, ciphertext, 16);
 }
@@ -37,6 +37,6 @@ void encrypt(const unsigned char plaintext[16], unsigned char ciphertext[16])
 void decrypt(const unsigned char ciphertext[16], unsigned char plaintext[16])
 {
 	rijndaelDecrypt(_rkd, _nrounds, ciphertext, plaintext);
-	xor(plaintext, _ivd);
+	xor( (unsigned int*) plaintext, (unsigned int*) _ivd);
 	memcpy(_ivd, ciphertext, 16);
 }
