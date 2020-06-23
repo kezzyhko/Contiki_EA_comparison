1728397ee3853ffc1b425daee49dc65d12a36039
bogotolec <i.makarenko@innopolis.university>
2020-06-23 18:24:50 +0300

    Changed mod of AES to CBC

--- cryptolibs/aes.c
+++ cryptolibs/aes.c
@@ -11,6 +11,9 @@ unsigned char _key[KEYLENGTH(MAX_KEYBITS)];
 int _keybits;
 int _nrounds;
 
+unsigned char _ive[32] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10 };
+unsigned char _ivd[32] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10 };
+
 void setKey(unsigned char* key, int keybits)
 {
 	_nrounds = rijndaelSetupEncrypt(_rke, key, keybits);
@@ -20,12 +23,25 @@ void setKey(unsigned char* key, int keybits)
 	memcpy(_key, key, KEYLENGTH(keybits));
 }
 
-void encrypt(const unsigned char plaintext[16], unsigned char ciphertext[16])
-{
-	rijndaelEncrypt(_rke, _nrounds, plaintext, ciphertext);
+void xor(unsigned char a[16], unsigned char b[16]) {
+	int i = 17;
+	while (--i > 0) {
+		*a^=*b;
+		++a;
+		++b;
+	}
 }
 
-void decrypt(const unsigned char ciphertext[16], unsigned char plaintext[16])
+void encrypt(unsigned char plaintext[16], unsigned char ciphertext[16])
+{
+	xor(plaintext, _ive);
+	rijndaelEncrypt(_rke, _nrounds, plaintext, ciphertext);
+	memcpy(_ive, ciphertext, 16);
+}
+
+void decrypt(unsigned char ciphertext[16], unsigned char plaintext[16])
 {
 	rijndaelDecrypt(_rkd, _nrounds, ciphertext, plaintext);
+	xor(plaintext, _ivd);
+	memcpy(_ivd, ciphertext, 16);
 }
