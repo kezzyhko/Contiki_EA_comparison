89c0338238b7157626fd0dec2fe18211efc2b94f
bogotolec <i.makarenko@innopolis.university>
2020-06-23 19:04:50 +0300

    Small optimization

--- cryptolibs/aes.c
+++ cryptolibs/aes.c
@@ -7,8 +7,6 @@
 
 unsigned long _rke[RKLENGTH(MAX_KEYBITS)];
 unsigned long _rkd[RKLENGTH(MAX_KEYBITS)];
-unsigned char _key[KEYLENGTH(MAX_KEYBITS)];
-int _keybits;
 int _nrounds;
 
 unsigned char _ive[32] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10 };
@@ -18,9 +16,6 @@ void setKey(unsigned char* key, int keybits)
 {
 	_nrounds = rijndaelSetupEncrypt(_rke, key, keybits);
 	_nrounds = rijndaelSetupDecrypt(_rkd, key, keybits);
-
-	_keybits = keybits;
-	memcpy(_key, key, KEYLENGTH(keybits));
 }
 
 void xor(unsigned char a[16], const unsigned char b[16]) {
