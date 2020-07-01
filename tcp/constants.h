33ed975f9cf0e3206e17fcd6844d0f647a65d316
Sergey Semushin <s.semushin@innopolis.university>
2020-07-01 15:09:00 +0300

    Added SPECK configuration to Makefile and constants.h

--- tcp/constants.h
+++ tcp/constants.h
@@ -8,9 +8,19 @@
 #define MESSAGE_CHARSET "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 #define MESSAGE_SIZE (10 * 1024)
 
-#define KEY "c0c0c0c0c0c0c0c0" //128bit
+// For PRESENT and SPECK:
+// do not forget to check ../cryptolibs/present.c or ../cryptolibs/speck.c files
+// and change configuration macros there
 
-// #define BLOCK_LENGTH 16 //128bit
-#define BLOCK_LENGTH 8 //64bit
+// #define BLOCK_LENGTH 4 //32bit
+// #define BLOCK_LENGTH 8 //64bit
+#define BLOCK_LENGTH 16 //128bit
+
+// #define KEY "c0c0c0c0" //64bit
+// #define KEY "c0c0c0c0c0" //80bit
+// #define KEY "c0c0c0c0c0c0" //96bit
+#define KEY "c0c0c0c0c0c0c0c0" //128bit
+// #define KEY "c0c0c0c0c0c0c0c0c0c0c0c0" //192bit
+// #define KEY "c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0" //256bit
 
 #endif /* CONSTANTS_H_ */
